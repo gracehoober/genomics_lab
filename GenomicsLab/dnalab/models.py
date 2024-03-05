fa30233aa5da02291a6c46c015077fb046b9ddd5
Grace Bethany Hoober <grace.hoober66@gmail.com>
2024-03-04 16:05:58 -0800

    added bones of models

--- GenomicsLab/dnalab/models.py
+++ GenomicsLab/dnalab/models.py
@@ -1,3 +1,36 @@
 from django.db import models
+import uuid
 
-# Create your models here.
+class Specimen(models.Model):
+    """DNA lab specimen class."""
+
+    id = models.
+    specimen_origin = models.CharField()
+    submission_date = models.DateTimeField()
+
+class Experiment(models.Model):
+    """Experiment class in DNA lab."""
+
+    name = models.CharField()
+    date_range = models.DateField()
+    lead_scientist = models.CharField()
+
+
+class Sequence(models.Model):
+    """DNA lab sequence class."""
+
+    id = models.CharField()
+    specimen = models.ForeignKey(Specimen, on_delete=models.CASCADE)
+    experiment = models.ForeignKey(Experiment, on_delete=models.CASCADE)
+
+
+class Scientist(models.Model):
+    """Scientist class in DNA lab."""
+
+    employee_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    first_name = models.CharField()
+    last_name = models.CharField()
+    seniority = models.
+    email = models.EmailField()
+
+

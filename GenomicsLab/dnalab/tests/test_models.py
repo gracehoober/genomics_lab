e3ec58dbba6326b06cab7b326ac5c77e1939cdce
Grace Bethany Hoober <grace.hoober66@gmail.com>
2024-04-01 16:33:02 -0700

    setup functions for model testing starte

--- GenomicsLab/dnalab/tests/test_models.py
+++ GenomicsLab/dnalab/tests/test_models.py
@@ -2,8 +2,27 @@ from django.test import TestCase
 from django.utils import timezone
 from ..models import Specimen, Sequence
 
+
 class SpecimenModelTest(TestCase):
     """Test cases for the Specimen Model."""
 
+    def setUp(self):
+        """Create a sample Specimen instance for testing."""
+
+        self.specimen = Specimen.objects.create(
+            specimen_origin="Test animal",
+            submission_date=timezone.now(),
+            notes="Test animal notes"
+        )
+
+
 class SequenceModelTest(TestCase):
     """Test cases for the Sequence Model."""
+
+    def setUp(self):
+        """Create a sample Sequence instance for testing."""
+
+        self.specimen = Sequence.objects.create(
+            dna_sequence="GATTACA",
+            specimen=
+        )

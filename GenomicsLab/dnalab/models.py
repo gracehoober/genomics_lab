48c5db425ac03c50b056d5c34ecf2f0b9af31f1f
Grace Bethany Hoober <grace.hoober66@gmail.com>
2024-04-01 15:11:20 -0700

    steup for model tests

--- GenomicsLab/dnalab/models.py
+++ GenomicsLab/dnalab/models.py
@@ -7,6 +7,7 @@ class Specimen(models.Model):
     specimen_origin = models.CharField(max_length=20)
     submission_date = models.DateTimeField()
     notes = models.TextField(max_length=500)
+    # TODO: create a list of all dna sequences
 
     def __str__(self):
         """Returns a string representation of a specimen."""
---
+++ GenomicsLab/dnalab/tests/test_models.py
@@ -0,0 +1,9 @@
+from django.test import TestCase
+from django.utils import timezone
+from ..models import Specimen, Sequence
+
+class SpecimenModelTest(TestCase):
+    """Test cases for the Specimen Model."""
+
+class SequenceModelTest(TestCase):
+    """Test cases for the Sequence Model."""

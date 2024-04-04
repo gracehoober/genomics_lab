d5b44236d056658bebe8d77a1c3237f877e381ef
Grace Bethany Hoober <grace.hoober66@gmail.com>
2024-04-03 17:03:59 -0700

    first model tests written

--- GenomicsLab/dnalab/tests/test_models.py
+++ GenomicsLab/dnalab/tests/test_models.py
@@ -10,19 +10,39 @@ class SpecimenModelTest(TestCase):
         """Create a sample Specimen instance for testing."""
 
         self.specimen = Specimen.objects.create(
-            specimen_origin="Test animal",
+            specimen_identifier="Test animal",
             submission_date=timezone.now(),
             notes="Test animal notes"
         )
+
+    def test_speciment_was_created(self):
+        """Tests a specimen was created."""
+
+        created_specimen = Specimen.objects.get(
+            specimen_identifier="Test Specimen")
+
+        self.assertEqual(isinstance(created_specimen, Specimen), True)
 
 
 class SequenceModelTest(TestCase):
     """Test cases for the Sequence Model."""
 
     def setUp(self):
-        """Create a sample Sequence instance for testing."""
+        """Create a sample Specimen and Sequence instance for testing."""
 
-        self.specimen = Sequence.objects.create(
-            dna_sequence="GATTACA",
-            specimen=
+        test_specimen = Specimen.objects.create(
+            specimen_identifier="Test Specimen",
+            submission_date=timezone.now(),
+            notes="notes about Test Specimen for testing a sequence."
         )
+        Sequence.objects.create(
+            dna_sequence="GATTACA", specimen=test_specimen
+        )
+
+    def test_seqence_was_created(self):
+        """Tests a sequence was created."""
+
+        created_sequence = Sequence.objects.get(
+            specimen_identifier="Test Specimen")
+
+        self.assertEqual(isinstance(created_sequence, Sequence), True)
